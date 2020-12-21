7de6b2bc572ba1ef3da69d75196ff36408313fe3
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-12-21 22:56:08 +0530

    Feat - Add API implementation

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ let btnTranslate = document.querySelector('#btn-translate');
 let txtInput = document.querySelector('#txt-input');
 let outputDiv = document.querySelector('#output');
 
+let serverURL = 'https://api.funtranslations.com/translate/minion.json';
+
+let getTranslation = text => {
+    return serverURL + '?text=' + text;
+};
+
+let errorHandler = error => {
+    console.error('Error :', error);
+    alert('Sorry, the server is down, please try some another time!');
+};
+
 let clickHandler = () => {
-    outputDiv.innerText = 'translated one ' + txtInput.value;
+    let inputText = txtInput.value;
+
+    fetch(getTranslation(inputText))
+        .then(response => response.json())
+        .then(json => {
+            let translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            console.log(json.contents.text);
+        })
+        .catch(error => errorHandler(error));
 };
 
 btnTranslate.addEventListener('click', clickHandler);
